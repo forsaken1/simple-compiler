1202d54ac17640f738ef7a2765519b644563ae15
forsaken <alexey2142@mail.ru>
2016-01-21 08:21:50 +1000

    Обработка первого теста

--- scanner.cr
+++ scanner.cr
@@ -1,7 +1,11 @@
-require 'token'
+require "./token"
 
 class Scanner
-  def initialize
-    @tokens = []
+  def initialize(@file_name)
+    @tokens = [] of Token
+  end
+
+  def run
+    @result = ""
   end
 end
--- tester.cr
+++ tester.cr
@@ -1,2 +1,28 @@
+require "./scanner"
+
 class Tester
+  TESTS_DIR = "tests"
+  SCANNER_TESTS_DIR = "#{TESTS_DIR}/scanner"
+  SCANNER_TEST_FILE_IN = "#{SCANNER_TESTS_DIR}/%s.in"
+  SCANNER_TEST_FILE_OUT = "#{SCANNER_TESTS_DIR}/%s.out"
+
+  def initialize
+    scanner_test
+  end
+
+  def scanner_test
+    (1..100).each do |i|
+      if File.exists?(SCANNER_TEST_FILE_IN % i)
+        if Scanner.new(SCANNER_TEST_FILE_IN % i).run == File.read(SCANNER_TEST_FILE_OUT % i)
+          puts '.'
+        else
+          puts 'E'
+        end
+      else
+        break
+      end
+    end
+  end
 end
+
+Tester.new
